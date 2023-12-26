2cf41ef793b86baa3455705ef8852a81a8e7c783
Bharat ruidas <bharatruidas261@gmail.com>
2023-12-26 15:45:51 +0530

    complete day 6

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -9,6 +9,8 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changeButton = false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -21,7 +23,7 @@ class _LoginPageState extends State<LoginPage> {
               height: 20,
             ),
             Text(
-              "Welcome to app",
+              "Welcome to $name",
               style: TextStyle(
                 fontWeight: FontWeight.bold,
                 fontSize: 20,
@@ -32,6 +34,10 @@ class _LoginPageState extends State<LoginPage> {
               child: Column(
                 children: [
                   TextFormField(
+                    onChanged: (val) {
+                      name = val;
+                      setState(() {});
+                    },
                     decoration: InputDecoration(
                         hintText: "enter your username",
                         labelText: "Username "),
@@ -45,14 +51,49 @@ class _LoginPageState extends State<LoginPage> {
                   SizedBox(
                     height: 20,
                   ),
-                  ElevatedButton(
-                    onPressed: () {
-                      print("hi bharat");
+                  InkWell(
+                    onTap: () async {
+                      
+                      setState(() {
+                        changeButton = true;
+                      });
+                      await Future.delayed(Duration(seconds: 1));
                       Navigator.pushNamed(context, MyRoutes.homeRoute);
                     },
-                    child: Text("submit"),
-                    style: TextButton.styleFrom(minimumSize: Size(300, 30)),
+                    child: AnimatedContainer(
+                      duration: Duration(seconds: 1),
+                      height: 50,
+                      width: changeButton ? 50 : 150,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple,
+                        // shape:
+                        //     changeButton ? BoxShape.circle : BoxShape.rectangle,
+                        borderRadius:
+                            BorderRadius.circular(changeButton ? 50 : 6),
+                      ),
+                      child: changeButton
+                          ? Icon(
+                              Icons.done,
+                              color: Colors.white,
+                            )
+                          : Text(
+                              "login",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                    ),
                   )
+                  // ElevatedButton(
+                  //   onPressed: () {
+                  //     print("hi bharat");
+                  //     );
+                  //   },
+                  //   child: Text("submit"),
+                  //   style: TextButton.styleFrom(minimumSize: Size(300, 30)),
+                  // )
                 ],
               ),
             ),
